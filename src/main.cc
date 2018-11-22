8b5cfbf9ebe7bd528e48afedaa3150c5296a6df9
John Blasco <jeblas@Lynx.local>
2018-11-22 12:59:23 -0500

    replaced tabs with spaces

--- src/main.cc
+++ src/main.cc
@@ -28,90 +28,90 @@ struct config_values {
 };
 
 block line_to_block(bool is_3d, std::string & line) {
-	    block out_block;
-	    std::string token;
+    block out_block;
+    std::string token;
 
-	    out_block.x = atoi(line.substr(0, line.find(',')).c_str());
-	    
-	    token = line.substr(line.find(',') + 1);
-	    out_block.y = atoi(token.substr(0, token.find(',')).c_str());
+    out_block.x = atoi(line.substr(0, line.find(',')).c_str());
+    
+    token = line.substr(line.find(',') + 1);
+    out_block.y = atoi(token.substr(0, token.find(',')).c_str());
 
-	    if (is_3d) {
-	        token = token.substr(token.find(',') + 1);
-                out_block.z = atoi(token.substr(0, token.find(',')).c_str());
-	    } else {
-	        out_block.z = 0;
-	    }
- 
-	    token = token.substr(token.find(',') + 1);
-            out_block.width = atoi(token.substr(0, token.find(',')).c_str());
-	    
-	    token = token.substr(token.find(',') + 1);
-            out_block.height = atoi(token.substr(0, token.find(',')).c_str());
+    if (is_3d) {
+        token = token.substr(token.find(',') + 1);
+            out_block.z = atoi(token.substr(0, token.find(',')).c_str());
+    } else {
+        out_block.z = 0;
+    }
 
-	    if (is_3d) {
-	        token = token.substr(token.find(',') + 1);
-                out_block.depth = atoi(token.substr(0, token.find(',')).c_str());
-	    } else {
-	        out_block.depth = 0;
-	    }
+    token = token.substr(token.find(',') + 1);
+        out_block.width = atoi(token.substr(0, token.find(',')).c_str());
+    
+    token = token.substr(token.find(',') + 1);
+        out_block.height = atoi(token.substr(0, token.find(',')).c_str());
 
-	    token = token.substr(token.find(',') + 1);
-            out_block.temp = atof(token.substr(0, token.find(',')).c_str());
+    if (is_3d) {
+        token = token.substr(token.find(',') + 1);
+            out_block.depth = atoi(token.substr(0, token.find(',')).c_str());
+    } else {
+        out_block.depth = 0;
+    }
 
-	    return out_block;
+    token = token.substr(token.find(',') + 1);
+        out_block.temp = atof(token.substr(0, token.find(',')).c_str());
+
+    return out_block;
 }
 
 void set_config_values(config_values & conf, std::string & file_name) {
-	    std::string buf;
-	    std::string token;
-	    uint8_t count = 0;
+    std::string buf;
+    std::string token;
+    uint8_t count = 0;
 
-	    std::ifstream conf_file(file_name);
-	    
-	    if (!conf_file) {
-                 std::cerr << "Error opening config file.\n";
-	    } else {
-		while (!conf_file.eof()){
-		    std::getline(conf_file, buf);
-		    // Filter out line that dont start with a number
-		    if(buf[0] != '#' && (buf[0] >= '0' && buf[0] <= '9')) {
-			switch (count) {
-			    case 0:
+    std::ifstream conf_file(file_name);
+    
+    if (!conf_file) {
+                std::cerr << "Error opening config file.\n";
+    } else {
+        while (!conf_file.eof()) {
+            std::getline(conf_file, buf);
+            // Filter out line that dont start with a number
+            if(buf[0] != '#' && (buf[0] >= '0' && buf[0] <= '9')) {
+            switch (count) {
+                case 0:
                                 conf.is_3d = (buf[0] == '3');
-				break;
-			    case 1:
-				conf.k = atof(buf.c_str());
-				break;
-			    case 2:
-				conf.num_timesteps = atoi(buf.c_str());
-				break;
-			    case 3: 
-				// GRID SIZE
-				conf.grid_width = atoi(buf.substr(0, buf.find(',')).c_str());
-			        
-				token = buf.substr(buf.find(',') + 1);
-				conf.grid_height = atoi(token.substr(0, token.find(',')).c_str());
-				
-				if (conf.is_3d) {
-				    conf.grid_depth = atoi(token.substr(token.find(',') + 1).c_str());
-				} else {
-				    conf.grid_depth = 0;
-				}
-				break;
-			    case 4:
-				conf.starting_temp = atof(buf.c_str());
-				break;
-			    default:
-				// PARSE THE REMAIN FILE FOR FIXED BLOCKS
-				conf.blocks.push_back(line_to_block(conf.is_3d, buf));
-			        break;	
-			}
-			++count;
-		    }
-		}
-	    }
-	    conf_file.close();
+                break;
+                case 1:
+                conf.k = atof(buf.c_str());
+                break;
+                case 2:
+                conf.num_timesteps = atoi(buf.c_str());
+                break;
+                case 3: 
+                // GRID SIZE
+                conf.grid_width = atoi(buf.substr(0, buf.find(',')).c_str());
+                    
+                token = buf.substr(buf.find(',') + 1);
+                conf.grid_height = atoi(token.substr(0, token.find(',')).c_str());
+                
+                if (conf.is_3d) {
+                    conf.grid_depth = atoi(token.substr(token.find(',') + 1).c_str());
+                } else {
+                    conf.grid_depth = 0;
+                }
+                break;
+                case 4:
+                conf.starting_temp = atof(buf.c_str());
+                break;
+                default:
+                // PARSE THE REMAIN FILE FOR FIXED BLOCKS
+                conf.blocks.push_back(line_to_block(conf.is_3d, buf));
+                    break;    
+            }
+            ++count;
+            }
+        }
+    }
+    conf_file.close();
 }
 
 
